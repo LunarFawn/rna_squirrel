7ec821963c6ac9b08702bdefcde66c65d230ce99
Jennifer Pearl <jenpearl5@gmail.com>
2023-10-19 23:27:00 -0700

    working on new yaml layout

--- src/rna_squirrel/config/yaml_objects.py
+++ src/rna_squirrel/config/yaml_objects.py
@@ -8,6 +8,10 @@ from dataclasses import dataclass, field
 from queue import PriorityQueue
 import heapq
 
+import sys
+import ruamel.yaml
+
+
 
 from rna_squirrel.config.dynamic_rna_strand import AtrClass
 
@@ -27,6 +31,13 @@ class NutObjectType(Enum):
     FLOATINGPOINT="FLOATINGPOINT"
     STRING="STRING"
     BOOLEAN="BOOLEAN"
+    DICTIONARY="DICTIONARY"
+    CONTAINER="CONTAINER"
+        
+    @classmethod
+    def from_yaml(cls, constructor, node):
+        return cls(*node.value.split('-'))
+    
 
         
 @dataclass
@@ -44,6 +55,7 @@ class NutObject():
     db_name:str = field(init=False)
     
     object_type: NutObjectType
+    object_info: str
     object_status: NutObjectStatus
     
       
@@ -59,27 +71,29 @@ class NutContainer:
     db_name:str = field(init=False)
     
     object_list: List[NutObject]
-    object_dict: Dict[str, NutObject]
+    #object_dict: Dict[str, NutObject]
     
     def __post_init__(self) -> None:
         self.db_name = f'{self.name}_db'
 
-
+@dataclass
+class NutDatabaseInfo:
+    db_name:str
      
 @dataclass
-class NutStructure():
+class NutStructure:
     """
     Represents the composition of the nut
     """
-    name:str
-    db_name:str = field(init=False)
+    db_info:NutDatabaseInfo
+    #db_name:str = field(init=False)
     nut_container_declarations:List[NutDeclaration]
     
     nut_main_struct:NutContainer    
-    nut_containers_dict:Dict[str,NutContainer]
+    nut_containers_list:List[NutContainer]
     
-    def __post_init__(self) -> None:
-        self.db_name = f'{self.name}_db'
+    #def __post_init__(self) -> None:
+    #    self.db_name = f'{self.name}_db'
 
 
     
