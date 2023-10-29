9adc9640e838d171cda1be02b1b2686f8cc594f7
Jennifer Pearl <jenpearl5@gmail.com>
2023-10-29 03:51:31 -0700

    backup

--- src/test/test_lori.py
+++ src/test/test_lori.py
@@ -8,7 +8,7 @@ def test_lori():
     new_lori:Lori = Lori(var_name='new_lori',
                      working_folder='/home/rnauser/repo/rna_squirrel/src/test/bin/show_lori/data')
     new_lori.sexy.boobs_names = "boobs"
-    new_lori.favorites.favorite_show.show_name = "TNG"
+    new_lori.favorites.favorite_show.show_name = "TN"
 
     print(f'{new_lori.sexy.boobs_names} are the returned name of loris boobs')
     print(f'{new_lori.favorites.favorite_show.show_name} is loris favorite show')
