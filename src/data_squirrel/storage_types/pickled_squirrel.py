a36d9a6ef440fb508294edd6fe624ef7d4963307
Jennifer Pearl <jenpearl5@gmail.com>
2024-07-02 20:22:20 -0700

    chipped a little piece off

--- src/data_squirrel/storage_types/pickled_squirrel.py
+++ src/data_squirrel/storage_types/pickled_squirrel.py
@@ -38,4 +38,15 @@ class PickelDataOperations(BasicDataOperations):
             raise Exception(f'Error while writing out {nut_name} to {data_path.target_path.as_posix()}')
     
     def read_data(self, working_folder:Path, nut_name:str, filename:Path):
-        pass
+        data_path:DataPathDetails = self.generate_data_path_details(working_folder=working_folder,
+                                                                    nut_name=nut_name,
+                                                                    filename=filename,
+                                                                    verify_working_folder=True)
+        data = None
+        try:
+            with data_path.target_path.open('wb') as file: 
+                data = pickle.load(file)
+        except:
+            raise Exception(f'Error while writing out {nut_name} to {data_path.target_path.as_posix()}')
+    
+        return data
