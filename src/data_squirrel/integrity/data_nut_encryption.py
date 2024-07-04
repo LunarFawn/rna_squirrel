4590eb944af614b8200d6a915c616f9021ba7c73
Jennifer Pearl <jenpearl5@gmail.com>
2024-07-04 14:36:37 -0700

    backup while building out the data integrity structure

---
+++ src/data_squirrel/integrity/data_nut_encryption.py
@@ -0,0 +1,3 @@
+"""
+This is the file for handeling the encryption of the data in a data nut
+"""
---
+++ src/data_squirrel/integrity/data_nut_validation.py
@@ -0,0 +1,4 @@
+"""
+This is the file that will control and handle operations that 
+validate the authenticiy and accuracy of data in a data nut
+"""
