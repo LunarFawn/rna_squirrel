a09d3d3cd37e098c09ba9d5c637807ac70f45157
Jennifer Pearl <jenpearl5@gmail.com>
2024-07-24 21:33:11 -0700

    backup while I work on stuff

--- src/data_squirrel/file_operations/file_manipulation/memory_manipulation.py
+++ src/data_squirrel/file_operations/file_manipulation/memory_manipulation.py
@@ -1,3 +1,20 @@
 """
 File for handeling manipulation of memory
 """
+
+import psutil
+# from psutil._pswindows import svmem
+# from psutil._pslinux import svmem
+from dataclasses import dataclass
+
+
+@dataclass
+class MemoryResources():
+    total_memory:int
+    free_memory:int
+    
+
+
+def get_memory_resources():
+    current_resources = psutil.virtual_memory()
+    
